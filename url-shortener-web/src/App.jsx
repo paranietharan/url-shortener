52af187a07493262f9d87b4c05b609190f8d1857
Paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-05-07 15:03:02 +0530

    Footer & Navbar created

--- url-shortener-web/src/App.jsx
+++ url-shortener-web/src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import About from './components/About'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
